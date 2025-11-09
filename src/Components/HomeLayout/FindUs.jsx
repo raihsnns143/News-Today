6688c74e089af9d73acbc784e8bee3e51bf985cf
Raihan Poramanik <raihanporamanik4@gmail.com>
2025-11-09 15:00:50 +0600

    home

--- src/Components/HomeLayout/FindUs.jsx
+++ src/Components/HomeLayout/FindUs.jsx
@@ -8,16 +8,36 @@ const FindUs = () => {
     <div>
       <div>
         <h3 className="font-bold mb-5">Find Us on</h3>
+
         <div className="join join-vertical w-full">
-          <button className="btn bg-base-100 justify-start join-item hover:bg-secondary hover:text-white">
+
+          <a
+            href="https://facebook.com/raihanns143"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn bg-base-100 justify-start join-item hover:bg-secondary hover:text-white"
+          >
             <MdFacebook size={25}/> Facebook
-          </button>
-          <button className="btn bg-base-100 justify-start join-item hover:bg-secondary hover:text-white">
+          </a>
+
+          <a
+            href="https://linkedin.com/company/yourcompany"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn bg-base-100 justify-start join-item hover:bg-secondary hover:text-white"
+          >
             <FaLinkedin size={20}/> Linkedin
-          </button>
-          <button className="btn bg-base-100 justify-start join-item hover:bg-secondary hover:text-white">
-            <BsInstagram size={20}/> Instragram
-          </button>
+          </a>
+
+          <a
+            href="https://instagram.com/raihanns143"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn bg-base-100 justify-start join-item hover:bg-secondary hover:text-white"
+          >
+            <BsInstagram size={20}/> Instagram
+          </a>
+
         </div>
       </div>
     </div>
