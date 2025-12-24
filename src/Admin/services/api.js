efc82498b142bc1b3510e87763369384e34d9cba
Raihan <raihanporamanik4@gmail.com>
2025-12-24 11:19:16 +0600

    fix api https and mixed content issue

--- src/Admin/services/api.js
+++ src/Admin/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://mominulweb.site/api";
+const API = "https://mominulweb.site/api";
 
 export const api = axios.create({
   baseURL: API,
@@ -8,6 +8,6 @@ export const api = axios.create({
 
 api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = token;
+  if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
